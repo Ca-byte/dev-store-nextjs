198deebcd9d8b6ec4cab133e6b58be8c2ae68fee
Caroline Vieira <carolinesanvieira@gmail.com>
2023-10-29 02:07:06 +0200

    app layout wip - parallel pages issues

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/header'
+import { ReactNode } from 'react'
+
+type StoreLayoutProps = {
+  children: ReactNode
+}
+
+export default function StoreLayout({ children }: StoreLayoutProps) {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  )
+}
