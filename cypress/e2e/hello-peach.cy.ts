70dead52d038d339deb5967cfaf53bb7c6990873
Caroline Vieira <carolinesanvieira@gmail.com>
2023-11-17 20:50:13 +0100

    cart e2e test

--- cypress/e2e/hello-peach.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://en.wikipedia.org/wiki/Now_and_Then_(Beatles_song)')
-    cy.contains('Beatles', { matchCase: false }).click()
-  })
-})
